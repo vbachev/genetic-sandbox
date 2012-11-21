6bc313731adc232ba702241d1854ac19690803aa
Vasil Bachev <vbachev4@gmail.com>
2012-11-21 15:48:16 +0200

    Update js/World.js
    minor refactoring

--- js/World.js
+++ js/World.js
@@ -1,97 +1,97 @@
 // kind of a singleton
-var World = (function () 
+var World = 
 {
-  var world = 
+  beatSpeed : 500,
+  handbrake : true, // will stop heartbeat at next beat
+
+  idPointer : 0,
+  subscribers : [], // holds all subscribers
+
+  // will move to cell manager object
+  familyTree : [], // will hold information on all cells
+
+  // starts heartbeat
+  start : function()
   {
-    beatSpeed : 500,
-    handbrake : true, // will stop heartbeat at next beat
+    console.log('world started turning');
+    this.handbrake = false;
+    this.heartbeat();
+  },
 
-    idPointer : 0,
-    beatSubscribers : [], // holds all subscribers
-    familyTree : [], // will hold information on all cells
+  // stops hartbeat
+  stop : function()
+  {
+    this.handbrake = true;
+    console.log('world stopped turning');
+  },
 
-    // starts heartbeat
-    start : function()
-    {
-      console.log('world started turning');
-      this.handbrake = false;
-      this.heartbeat();
-    },
-
-    // stops hartbeat
-    stop : function()
-    {
-      this.handbrake = true;
-      console.log('world stopped turning');
-    },
-
-    // handles the continuous beating
-    heartbeat : function(){
-      if( World.handbrake ){
-        return false;
-      }
-
-      World.beat();
-
-      setTimeout(
-        World.heartbeat,
-        World.beatSpeed
-      );
-    },
-
-    // handles publishing the beat event and updating all subscribers
-    beat : function()
-    {
-      var i, 
-      subscriber;
-
-      for( i in this.beatSubscribers ){
-        subscriber = this.beatSubscribers[i];
-        if( subscriber.update ){
-          subscriber.update();
-        }
-      }
-    },
-
-    // subscribe object for beat updates
-    register : function( a_object )
-    {
-      this.beatSubscribers.push( a_object );
-      this.idPointer++;
-      return this.idPointer;
-    },
-
-    unregister : function( a_id )
-    {
-      var i, 
-      subscriber;
-
-      for( i in this.beatSubscribers ){
-        subscriber = this.beatSubscribers[i];
-        if( a_id === subscriber.id ){
-          // remove item from array
-          this.beatSubscribers.splice( i, 1 );
-          return true;
-        }
-      }
-      
+  // handles the continuous beating
+  heartbeat : function(){
+    if( World.handbrake ){
       return false;
-    },
-
-    recordNewCell : function( a_cell )
-    {
-      this.familyTree.push( a_cell );
-    },
-
-    // describes the environment - just a prototype
-    getEnvironment : function()
-    {
-      return {
-        food : 5,
-        heat : 0
-      };
     }
 
-  };
-  return world;
-})();
+    World.beat();
+
+    setTimeout(
+      World.heartbeat,
+      World.beatSpeed
+    );
+  },
+
+  // handles publishing the beat event and updating all subscribers
+  beat : function()
+  {
+    var i, 
+    subscriber;
+
+    for( i in this.subscribers ){
+      subscriber = this.subscribers[i];
+      if( subscriber.update ){
+        subscriber.update();
+      }
+    }
+  },
+
+  // subscribe object for beat updates and return global id
+  register : function( a_object )
+  {
+    this.subscribers.push( a_object );
+
+    this.idPointer++;
+    return this.idPointer;
+  },
+
+  unregister : function( a_id )
+  {
+    var i, 
+    subscriber;
+
+    for( i in this.subscribers ){
+      subscriber = subscribers[i];
+      if( a_id === subscriber.id ){
+        // remove item from array
+        this.subscribers.splice( i, 1 );
+        return true;
+      }
+    }
+    
+    return false;
+  },
+
+  // will move to cell manager object
+  recordNewCell : function( a_cell )
+  {
+    this.familyTree.push( a_cell );
+  },
+
+  // will move to environment manager object
+  // describes the environment - just a prototype
+  getEnvironment : function()
+  {
+    return {
+      food : 5,
+      heat : 0
+    };
+  }
+};
