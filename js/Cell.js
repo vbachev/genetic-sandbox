ca82104fc94ab1cda5034898f28da1af6edbde73
Vasil Bachev <vbachev4@gmail.com>
2012-11-21 16:22:45 +0200

    refactoring of cell class

--- js/Cell.js
+++ js/Cell.js
@@ -1,8 +1,14 @@
 // cell class
+
+// TO DO:
+// - move cell inner process methods into prototype?
+
 function Cell ( a_config ) 
 {
   // subscribe for beat updates and get cell id
   this.id = World.register( this );
+  
+  // method will be moved to a different object
   World.recordNewCell( this ); // record in family tree :)
 
   // default constructor configuration
@@ -12,27 +18,30 @@ function Cell ( a_config )
 
   // vital stats
   this.alive  = true;
-  this.age    = 0;
+  this.age    = 0; // age (measured in the amount of world beats)
   this.growth = a_config.growth ? a_config.growth : 1;
-  this.health = 100;
+  this.health = 100; // like 100%
   this.food   = a_config.food ? a_config.food : 0;
 
   this.parentId = a_config.parentId ? a_config.parentId : 0
-  this.maturity = 10;
+  this.maturity = 10; // at what growth is it capable of reproducing
 
   this.update = function()
   {
     if( !this.alive ){
       return false;
     }
-
+    
+    // get environment data
     var environment = World.getEnvironment();
-
+    
+    // execute cell inner processes
     this.feed( environment );
     this.reproduce( environment );
     this.live();
   };
 
+  // implements cost of sustaining life
   this.live = function()
   {
     if( !this.alive ){
@@ -75,10 +84,13 @@ function Cell ( a_config )
   this.feed = function( a_environment )
   {
     if( a_environment.food > 0 ){
+      // some simple way to make larger cells aquire more food from the environment
+      // will be modified by genes
       this.food += Math.floor( this.growth / 5 ) + 1;
     }
   };
 
+  // check conditions and reproduce if possible
   this.reproduce = function( a_environment )
   {
     foodAvailable = a_environment.food && a_environment.food > 0 ? true : false;
