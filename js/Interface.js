84801558784ca09a420b593b6da4297904afcdd5
Vasil Bachev <vbachev4@gmail.com>
2012-11-21 15:56:56 +0200

    minor refactoring on interface

--- js/Interface.js
+++ js/Interface.js
@@ -1,38 +1,44 @@
-function Interface()
+var Interface =
 {
-  if ( arguments.callee._singletonInstance ){
-    return arguments.callee._singletonInstance;
-  }
-  arguments.callee._singletonInstance = this;
+  // subscribe for beat updates
+  id : World.register( this ),
 
-  this.id = World.register( this );
-
-  this.cellsList = $('#cells');
-
-  this.update = function()
+  // must initialize after document is ready to be able 
+  // to detect this dom node
+  initialize : function(){
+    this.cellsList = $('#cells');
+  },
+  
+  // at each beat go through all cells and draw them
+  update : function()
   {
     var cells = World.familyTree,
     i, cell, node,
-    toAppend = '';
+    toAppend = '',
+    modifier = 0;
 
     for( i in cells ){
       cell = cells[i];
       node = $('#cell'+cell.id);
       
       if( cell.alive ){
+        // use the cell growth value as an indicator
         modifier = cell.growth;
       } else {
         node.addClass('dead');
         continue;
       }
-
+  
+      // edit an existing node or create a new one
+      // font-size will affect all css properties that use em units
       if( node.length ){
         node.css('font-size', modifier+'px');
       } else {
         toAppend += '<li id="cell'+cell.id+'" style="font-size:'+modifier+'px"></li>';
       }
-    }
-
+    },
+    
+    // add new items to list
     this.cellsList.append( toAppend );
-  };
+  }
 };
